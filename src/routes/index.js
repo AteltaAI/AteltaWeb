cff7ce90a878418837931c42447e4d3d70140e85
Siddhant Prateek <siddhantprateek@gmail.com>
2023-03-02 13:52:27 +0530

    basic application routes added ✅

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,3 +2,5 @@
 export { default as Home } from './home/home.routes';
 export { default as Dashboard } from './dashboard/dashboard.routes';
 export { default as Demo } from './demo/demo.routes';
+export { default as Developers } from './developers/developers.routes';
+export { default as Solutions } from './solutions/solutions.routes';

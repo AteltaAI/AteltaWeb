8233e8ce8009f7a50cd59db75e6fec87e4d4a169
Siddhant Prateek <siddhantprateek@gmail.com>
2023-03-07 08:51:07 +0530

    hero upload component for ateltaWeb

--- src/components/hero/hero.components.jsx
+++ src/components/hero/hero.components.jsx
@@ -4,6 +4,16 @@ import './hero.styles.css';
 const Hero = () => {
   return (
     <div className='hero-div'>
+
+      <div className="hero-content">
+        <div className="upload-btn">
+          <button className='custom-upload'>UPLOAD FILE / PASTE THE YOUTUBE LINK TO DOWNLOAD</button>
+        </div>
+        <div className="secondary-content">
+          <p>We are processing your video in the mean time, please checkout our existing samples</p>
+          <button>PRACTISE</button>
+        </div>
+      </div>
     </div>
   )
 }
